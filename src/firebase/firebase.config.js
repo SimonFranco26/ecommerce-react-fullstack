1bcc2e3a51f2d1d5cacdd65c49715fc518ad23a8
SimonFranco26 <simonfranco323015@correo.itm.edu.co>
2026-05-11 19:59:16 -0500

    fix: agregar configuración de firebase y github pages

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLToGUcmvGdH8OHqoIRNSTx5AWjImfTH0",
+  authDomain: "ecommerce-react-62a5a.firebaseapp.com",
+  projectId: "ecommerce-react-62a5a",
+  storageBucket: "ecommerce-react-62a5a.firebasestorage.app",
+  messagingSenderId: "551256431950",
+  appId: "1:551256431950:web:cf2c470b696d22af44d21f",
+  measurementId: "G-MWJQMLB53R"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
